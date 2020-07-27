2b61e19ef1010e3c0ad9e5eb0e2a13a0885e8596
Gavin <gavin@atech.media>
2020-07-27 12:47:02 +0100

    Install vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,45 @@
 // webpack.config.js
 var webpack = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   entry: {
     site: "./source/javascripts/site.js",
   },
+  plugins: [new VueLoaderPlugin()],
   output: {
     path: __dirname + "/.tmp/dist",
     filename: "source/javascripts/[name].js",
   },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+    extensions: ["*", ".js", ".vue", ".json"],
+  },
   module: {
     rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          // vue-loader options go here
+        },
+      },
+      {
+        test: /\.pug$/,
+        oneOf: [
+          // this applies to `<template lang="pug">` in Vue components
+          {
+            resourceQuery: /^\?vue/,
+            use: ["pug-plain-loader"],
+          },
+          // this applies to pug imports inside JavaScript
+          {
+            use: ["raw-loader", "pug-plain-loader"],
+          },
+        ],
+      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
