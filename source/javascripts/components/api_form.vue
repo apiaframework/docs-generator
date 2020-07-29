8cb99ba874814834d8375b2a8cf96be8f2eda144
Gavin <gavin@atech.media>
2020-07-29 09:34:21 +0100

    Generate a values hash for the args

--- source/javascripts/components/api_form.vue
+++ source/javascripts/components/api_form.vue
@@ -9,5 +9,23 @@ export default {
       type: Array,
     },
   },
+  data: function() {
+    return {
+      values: this.emptyValues(this.args),
+    };
+  },
+  methods: {
+    emptyValues: function(args) {
+      let vals = {};
+      args.forEach((arg) => {
+        if (arg.arguments) {
+          vals[arg.name] = this.emptyValues(arg.arguments);
+        } else {
+          vals[arg.name] = "";
+        }
+      });
+      return vals;
+    },
+  },
 };
 </script>
