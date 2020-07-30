d97cddb9b7ee346a90ac7b248775c9b1e2c30966
Gavin <gavin@atech.media>
2020-07-30 09:42:43 +0100

    Implement URL generation

--- source/javascripts/components/api_demo.vue
+++ source/javascripts/components/api_demo.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
   div
-    span {{method}}
-    APIForm(v-bind:args="this.args")
+    APIForm(v-bind:args="this.args" v-on:submit="formSubmitted")
 </template>
 <script>
 import APIForm from "./api_form.vue";
@@ -16,6 +15,33 @@ export default {
     },
     method: {
       type: String,
+    },
+    url: {
+      type: Object,
+    },
+  },
+  methods: {
+    formSubmitted: function(values) {
+      const url = this.generateURL(values);
+    },
+    generateURL: function(values) {
+      let pathWithArgs = this.url.path;
+      const matches = Array.from(
+        this.url.path.matchAll(/\:(\w+)/g),
+        (m) => m[0]
+      );
+      const urlArgs = matches.forEach((urlArg) => {
+        const argName = urlArg.substr(1);
+        let argValue = "";
+        Object.values(values[argName]).forEach((val) => {
+          if (val) {
+            argValue = val;
+          }
+        });
+        pathWithArgs = pathWithArgs.replace(urlArg, argValue);
+      });
+
+      return `https://${this.url.host}${this.url.namespace}/${pathWithArgs}`;
     },
   },
 };
